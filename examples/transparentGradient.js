8fd9e9e10f2590034a08c0f2d0c9ced3f2eb29bc
Mr.chu <chuhongkyu@gmail.com>
2023-06-10 22:32:52 +0900

    Add transparent gradient

--- examples/transparentGradient.js
+++ examples/transparentGradient.js
@@ -1,22 +1,5 @@
+import tGradient from "..transparentGradient.bundle"
+
 //module
 const content2 = document.querySelector('.number2')
 tGradient(content2)
-
-function tGradient(element, direction = 'right') {
-    if (!element) {
-      throw new Error('element is required');
-    }
-  
-    const validDirections = ['right', 'bottom', 'top', 'left'];
-    const normalizedDirection = validDirections.includes(direction) ? direction : 'right';
-  
-    element.setAttribute('style', `
-      -webkit-mask-size: cover;
-      mask-size: cover;
-      -webkit-mask-repeat: no-repeat;
-      -webkit-mask-position: ${normalizedDirection};
-      mask-repeat: no-repeat;
-      -webkit-mask-image: linear-gradient(to ${normalizedDirection},black, transparent), linear-gradient(to ${normalizedDirection},black,transparent);
-      mask-image: linear-gradient(to ${normalizedDirection},black, transparent), linear-gradient(to ${normalizedDirection},black,transparent);
-    `);
-}
