df30938f06d4a2ea555a19a1855bd5da8f37cbaf
Mr.chu <chuhongkyu@gmail.com>
2023-09-03 01:47:22 +0900

    Add ts

--- src/transparentGradient.ts
+++ src/transparentGradient.ts
@@ -1,28 +1,30 @@
 import { ITarget } from "./tGradientType";
 
-const tGradient = ({target, direction}:ITarget) => {
-  let main;
-  if (!target) {
-    throw new Error('target is required');
-  }
-  main = target;
-  const validDirections = ['right', 'bottom', 'top', 'left'];
-  const normalizedDirection = validDirections.includes(direction) ? direction : 'right';
+const tGradient = ({ target, direction }: ITarget) => {
+  let main: Element | null = null; // main을 Element 또는 null로 타입 지정
 
-  if(typeof target == "string"){
-    main = document.querySelector(target)
-  }else{
-    main = target
+  if (typeof target === "string") {
+    main = document.querySelector(target);
+  } else {
+    main = target;
   }
+
+  if (!main) {
+    throw new Error('target is required or invalid');
+  }
+
+  const validDirections = ['right', 'bottom', 'top', 'left'];
+  const normalizedDirection = direction && validDirections.includes(direction) ? direction : 'right';
+  
   main.setAttribute('style', `
     -webkit-mask-size: cover;
     mask-size: cover;
     -webkit-mask-repeat: no-repeat;
     -webkit-mask-position: ${normalizedDirection};
     mask-repeat: no-repeat;
-    -webkit-mask-image: linear-gradient(to ${normalizedDirection},black, transparent), linear-gradient(to ${normalizedDirection},black,transparent);
-    mask-image: linear-gradient(to ${normalizedDirection},black, transparent), linear-gradient(to ${normalizedDirection},black,transparent);
+    -webkit-mask-image: linear-gradient(to ${normalizedDirection}, black, transparent), linear-gradient(to ${normalizedDirection}, black, transparent);
+    mask-image: linear-gradient(to ${normalizedDirection}, black, transparent), linear-gradient(to ${normalizedDirection}, black, transparent);
   `);
-}
+};
 
-export default tGradient
+export default tGradient;
--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,21 @@ const path = require('path');
 
 module.exports = {
   entry: './src/transparentGradient.ts',
-  devtool: "inline-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
+  module: {
+      rules: [
+          {
+              test: /\.tsx?$/,
+              use: "ts-loader",
+              exclude: /node_modules/,
+          },
+      ],
+  },
   resolve: {
       extensions: [".tsx", ".ts", ".js"],
   },
   output: {
     filename: "transparentGradient.js",
     path: path.resolve(__dirname, 'lib'),
-    globalObject: 'this',
-    libraryTarget: 'umd',
   },
   optimization: {
     minimize: false, // 압축 비활성화
